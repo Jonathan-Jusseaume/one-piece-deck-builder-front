493352dae076bfcb54432931b53d07f0b2fe0585
JotaroE21 <jusseaumejonathan5@gmail.com>
2022-08-25 12:30:37 +0200

    making multiple images transition

--- src/app/shared/component/card/card.component.ts
+++ src/app/shared/component/card/card.component.ts
@@ -1,13 +1,14 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {ConfigurationService} from "../../service/configuration.service";
 import {TypeEnum} from "../../model/constant/TypeEnum";
+import {interval, Subscription} from "rxjs";
 
 @Component({
   selector: 'opdb-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnInit, OnDestroy {
 
   @Input()
   public card: Card;
@@ -18,17 +19,28 @@ export class CardComponent implements OnInit {
   @Input()
   public countInDeck: number = 0;
 
+  public currentImageIndex = 0;
+
   @Output()
   public cardIsClicked: EventEmitter<Card> = new EventEmitter<Card>();
+
+  private subscriptions: Subscription[] = [];
 
   TypeEnum = TypeEnum;
 
   constructor(public _configurationService: ConfigurationService) { }
 
   ngOnInit(): void {
+    this.subscriptions.push(interval(3000).subscribe(() => {
+      this.currentImageIndex =(this.currentImageIndex + 1) % (this.card.images.length)
+    }))
   }
 
   isClicked(card: Card) {
     this.cardIsClicked.emit(card);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions?.forEach(sub => sub.unsubscribe());
   }
 }
