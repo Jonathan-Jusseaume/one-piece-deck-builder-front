8f921938de2e5497c6496a373e24b89b2250ec62
JotaroE21 <jusseaumejonathan5@gmail.com>
2022-12-15 20:32:07 +0100

    Beginning holo cards

--- src/app/shared/component/card/card.component.ts
+++ src/app/shared/component/card/card.component.ts
@@ -35,6 +35,8 @@ export class CardComponent implements OnInit, OnDestroy {
 
     private subscriptions: Subscription[] = [];
 
+    public movementTest = '';
+
     constructor(public _configurationService: ConfigurationService, public _colorService: ColorService) {
     }
 
@@ -55,5 +57,49 @@ export class CardComponent implements OnInit, OnDestroy {
 
     eyeClick(card: Card): void {
         this.cardEyeIsClicked.emit(card);
+    }
+
+    cardAnimation($event: MouseEvent): void {
+        // normalise touch/mouse
+        let pos = [$event.offsetX, $event.offsetY];
+        $event.preventDefault();
+        /*
+        if ($event.type === "touchmove") {
+            pos = [$event.touches[0].clientX, $event.touches[0].clientY];
+        }
+        */
+        const cardImage = document.getElementById(this.card?.id);
+        console.log(cardImage);
+        // math for mouse position
+        const l = pos[0];
+        const t = pos[1];
+        const h = (cardImage as HTMLImageElement).height;
+        const w = (cardImage as HTMLImageElement).width;
+        const px = Math.abs(Math.floor(100 / w * l) - 100);
+        const py = Math.abs(Math.floor(100 / h * t) - 100);
+        const pa = (50 - px) + (50 - py);
+        const lp = (50 + (px - 50) / 1.5);
+        const tp = (50 + (py - 50) / 1.5);
+        const px_spark = (50 + (px - 50) / 7);
+        const py_spark = (50 + (py - 50) / 7);
+        const p_opc = 20 + (Math.abs(pa) * 1.5);
+        const ty = ((tp - 50) / 2) * -1;
+        const tx = ((lp - 50) / 1.5) * .5;
+        // css to apply for active card
+        const grad_pos = `background-position: ${lp}% ${tp}%;`
+        const sprk_pos = `background-position: ${px_spark}% ${py_spark}%;`
+        const opc = `opacity: ${p_opc / 100};`
+        this.movementTest = `transform: rotateX(${ty}deg) rotateY(${tx}deg)`
+        // need to use a <style> tag for psuedo elements
+        const style = `
+      .card:hover:before { ${grad_pos} }  /* gradient */
+      .card:hover:after { ${sprk_pos} ${opc} }   /* sparkles */ 
+    `
+
+
+    }
+
+    cardAnimationEnd($event: MouseEvent) {
+        this.movementTest = '';
     }
 }
